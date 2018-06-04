f8c0c134c48939b3759bdca3abb97522b0254d57
Richard Wurth <rwurth@communitect.com>
2018-06-03 21:50:49 -0600

    Removed status, combined status into sliding switch

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
   let squareStatus = 'off';
-  $('#square').click(function(){
+  $('#bar-parent').click(function(){
     if (squareStatus === 'off') {
-    $('#square').css('right','-20px');
-    $('#text').css('background-color','green');
-    $('#text').text('Active');
+    $('#square').css('right','-8%');
+    $('#on').css('right','5%');
+    $('#off').css('left','100%');
+    // $('#text').css('background-color','green');
+    // $('#text').text('Active');
     squareStatus = 'on';
       } else {
-    $('#square').css('right','20px');
-    $('#text').css('background-color','red');
-    $('#text').text('Inactive');
+    $('#square').css('right','8%');
+    $('#on').css('right','100%');
+    $('#off').css('left','5%');
+    // $('#text').css('background-color','red');
+    // $('#text').text('Inactive');
+    squareStatus = 'off';
+    }
+  });
+  $('#square').click(function(){
+    if (squareStatus === 'off') {
+    $('#square').css('right','-8%');
+    $('#on').css('right','5%');
+    $('#off').css('left','100%');
+    // $('#text').css('background-color','green');
+    // $('#text').text('Active');
+    squareStatus = 'on';
+      } else {
+    $('#square').css('right','8%');
+    $('#on').css('right','100%');
+    $('#off').css('left','5%');
+    // $('#text').css('background-color','red');
+    // $('#text').text('Inactive');
     squareStatus = 'off';
     }
   });
