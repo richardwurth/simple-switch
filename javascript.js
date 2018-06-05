7ea5a17962e8d8af90e10057cfb6980d460e1130
Richard Wurth <rwurth@communitect.com>
2018-06-04 20:49:27 -0600

    Initial commit of Minimalistic Switch

--- javascript.js
+++ javascript.js
@@ -1,36 +1,26 @@
 $(document).ready(function(){
   let squareStatus = 'off';
-  $('#bar-parent').click(function(){
-    if (squareStatus === 'off') {
-    $('#square').css('right','-8%');
-    $('#on').css('right','5%');
-    $('#off').css('left','100%');
-    // $('#text').css('background-color','green');
-    // $('#text').text('Active');
-    squareStatus = 'on';
-      } else {
-    $('#square').css('right','8%');
-    $('#on').css('right','100%');
-    $('#off').css('left','5%');
-    // $('#text').css('background-color','red');
-    // $('#text').text('Inactive');
-    squareStatus = 'off';
-    }
-  });
   $('#square').click(function(){
     if (squareStatus === 'off') {
-    $('#square').css('right','-8%');
+      $('#square').css('right','-15%');
+      $('#on').css('right','5%');
+      $('#off').css('left','100%');
+    } else {
+      $('#square').css('right','70%');
+      $('#on').css('right','100%');
+      $('#off').css('left','5%');
+    }
+  });
+  $('#bar-parent').click(function(){
+    if (squareStatus === 'off') {
+    $('#square').css('right','-15%');
     $('#on').css('right','5%');
     $('#off').css('left','100%');
-    // $('#text').css('background-color','green');
-    // $('#text').text('Active');
     squareStatus = 'on';
       } else {
-    $('#square').css('right','8%');
+    $('#square').css('right','70%');
     $('#on').css('right','100%');
     $('#off').css('left','5%');
-    // $('#text').css('background-color','red');
-    // $('#text').text('Inactive');
     squareStatus = 'off';
     }
   });
